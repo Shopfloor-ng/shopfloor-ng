541c2c1596a9281db3dbb8ce24a151eea9ba114a
Davide Del Popolo <davidedp91@gmail.com>
2021-08-31 11:31:25 +0200

    add step model

---
+++ src/app/model/step.ts
@@ -0,0 +1,8 @@
+export class Step {
+    id: string = '';
+    number: number = 0;
+    description: string = '';
+    previousStep: string = '';
+    nextStep: string = '';
+    module: string = '';
+}
