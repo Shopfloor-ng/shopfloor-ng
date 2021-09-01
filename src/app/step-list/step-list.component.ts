c9b4aca8d1525535e66996614491fb74da570bc9
Davide Del Popolo <davidedp91@gmail.com>
2021-09-01 10:43:15 +0200

    fix search on step-list

--- src/app/step-list/step-list.component.ts
+++ src/app/step-list/step-list.component.ts
@@ -8,14 +8,28 @@ import { Step } from '../model/step';
   styleUrls: ['./step-list.component.scss']
 })
 export class StepListComponent implements OnInit {
-
+  filter: string | undefined;
   steps: Step[] = [];
   constructor(private api: ApiService) { }
 
+  EnterSubmit(event: { keyCode: number; }, form: any) {
+    //keycode for Enter is 13 
+    if (event.keyCode === 13) {
+        //calling submit method if key pressed is Enter.
+        this.SubmitForm(form);
+    }
+}
+  SubmitForm(form: any) {
+    // console.log(form.value);
+    this.filter = form.value.searchbox;
+    this.refreshSteps();
+  }
+
   refreshSteps() {
-    this.api.getStep()
+    // console.log(this.filter);
+    this.api.getStep(undefined,undefined,this.filter)
       .subscribe(data => {
-        console.log(data)
+        // console.log(data)
         this.steps = data;
       })
   }
