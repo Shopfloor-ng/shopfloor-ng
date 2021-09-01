f492fd36e6fcfb07e185cb03b63216b375a05f6a
Davide Del Popolo <davidedp91@gmail.com>
2021-09-01 15:27:22 +0200

    step detail base

--- src/app/step-detail/step-detail.component.ts
+++ src/app/step-detail/step-detail.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Step } from '../model/step';
 
 @Component({
   selector: 'app-step-detail',
   templateUrl: './step-detail.component.html',
   styleUrls: ['./step-detail.component.scss']
 })
-export class StepDetailComponent implements OnInit {
+export class StepDetailComponent implements OnChanges {
 
+  @Input() inputStep?: Step;
+  step: Step = new Step();
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.inputStep!!) {
+      console.log(this.inputStep);
+      this.loadStepFromId(this.inputStep);
+    }
+  }
+
+  loadStepFromId(step: Step) {
+    if (this.inputStep!!) {
+      this.step = this.inputStep;
+    }
   }
 
 }
+
